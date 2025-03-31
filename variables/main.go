71d036877a5b972bfaac28bac93106bc73d490c7
serbina <serbina@chalmers.net.se>
2025-03-31 11:41:02 +0200

    Use variables

---
+++ variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import  (
+	"fmt"
+	"math"
+)
+
+func main(){
+	var a int = 10
+	var b = 12
+	string1, string2 := "Hi", "There"
+	min := math.Min(10, 12)
+	fmt.Println("a is", a)
+	fmt.Println("b is", b)
+	fmt.Println(string1, string2)
+	fmt.Println("min is", min)
+}
